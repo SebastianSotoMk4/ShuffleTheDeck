7df116c71ada5b21ec2db708db1c7bf0e5f8c803
Sebastian Soto <thefirstcoolone@gmail.com>
2021-09-28 09:03:23 -0600

    title Block and links

---
+++ ShuffleTheDeck/ShuffleTheDeck.vb
@@ -0,0 +1,15 @@
+﻿'Sebastian Soto
+'RCET0265
+'Fall 2021
+'Shuffle The Deck
+'https://github.com/SebastianSotoMk4/ShuffleTheDeck.git
+Option Strict On
+Option Explicit On
+Module ShuffleTheDeck
+
+    Sub Main()
+
+    End Sub
+
+End Module
+'https://elearn.isu.edu/moodle/mod/assign/view.php?id=2180761
