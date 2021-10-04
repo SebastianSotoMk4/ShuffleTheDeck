94352f7d109cf691361cda7fff067ffb0184719f
Sebastian Soto <thefirstcoolone@gmail.com>
2021-10-03 19:35:49 -0600

    works

--- ShuffleTheDeck/ShuffleTheDeck.vb
+++ ShuffleTheDeck/ShuffleTheDeck.vb
@@ -1,109 +1,107 @@
-﻿'Sebastian Soto
+﻿
+'Sebastian Soto
 'RCET0265
 'Fall 2021
 'Shuffle The Deck
 'https://github.com/SebastianSotoMk4/ShuffleTheDeck.git
 Option Strict On
 Option Explicit On
+Option Compare Text
 Module ShuffleTheDeck
-
     Sub Main()
-
         Dim cardReturn As String
+        Dim aNumber As String
         Dim shuffleCheck As String
-        Dim cardDeck(3, 12) As String
-        Dim cardCheck(3, 12) As String
-        Dim suit As Integer
+        Dim cardDeck(51) As String
+        Dim cardCheck(51) As String
         Dim value As Integer
         Dim shuffle As Boolean
+        Dim quit As Boolean
 
-        cardDeck(0, 0) = "Club, ace"
-        cardDeck(0, 1) = "Club, 2"
-        cardDeck(0, 2) = "Club, 3"
-        cardDeck(0, 3) = "Club, 4"
-        cardDeck(0, 4) = "Club, 5"
-        cardDeck(0, 5) = "Club, 6"
-        cardDeck(0, 6) = "Club, 7"
-        cardDeck(0, 7) = "Club, 8"
-        cardDeck(0, 8) = "Club, 9"
-        cardDeck(0, 9) = "Club, 10"
-        cardDeck(0, 10) = "Club, Jack"
-        cardDeck(0, 11) = "Club, Queen"
-        cardDeck(0, 12) = "Club, king"
-
-        cardDeck(1, 0) = "Hearts, Ace"
-        cardDeck(1, 1) = "Hearts, 2"
-        cardDeck(1, 2) = "Hearts, 3"
-        cardDeck(1, 3) = "Hearts, 4"
-        cardDeck(1, 4) = "Hearts, 5"
-        cardDeck(1, 5) = "Hearts, 6"
-        cardDeck(1, 6) = "Hearts, 7"
-        cardDeck(1, 7) = "Hearts, 8"
-        cardDeck(1, 8) = "Hearts, 9"
-        cardDeck(1, 9) = "Hearts, 10"
-        cardDeck(1, 10) = "Hearts, Jack"
-        cardDeck(1, 11) = "Hearts, Queen"
-        cardDeck(1, 12) = "Hearts, king"
-
-        cardDeck(2, 0) = "Spades, Ace"
-        cardDeck(2, 1) = "Spades, 2"
-        cardDeck(2, 2) = "Spades, 3"
-        cardDeck(2, 3) = "Spades, 4"
-        cardDeck(2, 4) = "Spades, 5"
-        cardDeck(2, 5) = "Spades, 6"
-        cardDeck(2, 6) = "Spades, 7"
-        cardDeck(2, 7) = "Spades, 8"
-        cardDeck(2, 8) = "Spades, 9"
-        cardDeck(2, 9) = "Spades, 10"
-        cardDeck(2, 10) = "Spades, Jack"
-        cardDeck(2, 11) = "Spades, Queen"
-        cardDeck(2, 12) = "Spades, king"
-
-        cardDeck(3, 0) = "Diamond, Ace"
-        cardDeck(3, 1) = "Diamond, 2"
-        cardDeck(3, 2) = "Diamond, 3"
-        cardDeck(3, 3) = "Diamond, 4"
-        cardDeck(3, 4) = "Diamond, 5"
-        cardDeck(3, 5) = "Diamond, 6"
-        cardDeck(3, 6) = "Diamond, 7"
-        cardDeck(3, 7) = "Diamond, 8"
-        cardDeck(3, 8) = "Diamond, 9"
-        cardDeck(3, 9) = "Diamond, 10"
-        cardDeck(3, 10) = "Diamond, Jack"
-        cardDeck(3, 11) = "Diamond, Queen"
-        cardDeck(3, 12) = "Diamond, king"
+        cardDeck(0) = "Club, ace"
+        cardDeck(1) = "Club, 2"
+        cardDeck(2) = "Club, 3"
+        cardDeck(3) = "Club, 4"
+        cardDeck(4) = "Club, 5"
+        cardDeck(5) = "Club, 6"
+        cardDeck(6) = "Club, 7"
+        cardDeck(7) = "Club, 8"
+        cardDeck(8) = "Club, 9"
+        cardDeck(9) = "Club, 10"
+        cardDeck(10) = "Club, Jack"
+        cardDeck(11) = "Club, Queen"
+        cardDeck(12) = "Club, king"
+        cardDeck(13) = "Hearts, Ace"
+        cardDeck(14) = "Hearts, 2"
+        cardDeck(15) = "Hearts, 3"
+        cardDeck(16) = "Hearts, 4"
+        cardDeck(17) = "Hearts, 5"
+        cardDeck(18) = "Hearts, 6"
+        cardDeck(19) = "Hearts, 7"
+        cardDeck(20) = "Hearts, 8"
+        cardDeck(21) = "Hearts, 9"
+        cardDeck(22) = "Hearts, 10"
+        cardDeck(23) = "Hearts, Jack"
+        cardDeck(24) = "Hearts, Queen"
+        cardDeck(25) = "Hearts, king"
+        cardDeck(26) = "Spades, Ace"
+        cardDeck(27) = "Spades, 2"
+        cardDeck(28) = "Spades, 3"
+        cardDeck(29) = "Spades, 4"
+        cardDeck(30) = "Spades, 5"
+        cardDeck(31) = "Spades, 6"
+        cardDeck(32) = "Spades, 7"
+        cardDeck(33) = "Spades, 8"
+        cardDeck(34) = "Spades, 9"
+        cardDeck(35) = "Spades, 10"
+        cardDeck(36) = "Spades, Jack"
+        cardDeck(37) = "Spades, Queen"
+        cardDeck(38) = "Spades, king"
+        cardDeck(39) = "Diamond, Ace"
+        cardDeck(40) = "Diamond, 2"
+        cardDeck(41) = "Diamond, 3"
+        cardDeck(42) = "Diamond, 4"
+        cardDeck(43) = "Diamond, 5"
+        cardDeck(44) = "Diamond, 6"
+        cardDeck(45) = "Diamond, 7"
+        cardDeck(46) = "Diamond, 8"
+        cardDeck(47) = "Diamond, 9"
+        cardDeck(48) = "Diamond, 10"
+        cardDeck(49) = "Diamond, Jack"
+        cardDeck(50) = "Diamond, Queen"
+        cardDeck(51) = "Diamond, king"
         Do
+            Console.WriteLine("Press enter to begin dealing cards")
+            Console.ReadLine()
+            shuffle = False
+            For i = 1 To 52
+                Do
+                    value = DealACard(52)
+                    aNumber = cardCheck(value)
+                Loop Until aNumber = Nothing
 
-            For i = 1 To 55
-                suit = DealACard(4)
-                value = DealACard(12)
-                Select Case cardCheck(suit, value)
-                    Case = "retry"
-                        cardReturn = "retry"
-                    Case <> "retry"
-                        cardReturn = cardDeck(suit, value)
-                        cardCheck(suit, value) = "retry"
-                        Console.WriteLine(cardReturn)
-                        Console.ReadLine()
-                End Select
+                cardReturn = cardDeck(value)
+                cardCheck(value) = cardDeck(value)
+                Console.WriteLine(cardReturn)
+                'Console.ReadLine()
             Next
-            Console.WriteLine("shuffle? Y / N ")
-            shuffleCheck = Console.ReadLine()
-            Select Case shuffleCheck
-                Case = "Y"
-                    For i = 0 To 3
-                        For u = 0 To 12
-                            cardCheck(i, u) = " "
+            Console.WriteLine("Type S to shuffle or Q to quit")
+            Do
+                shuffleCheck = Console.ReadLine()
+                Select Case shuffleCheck
+                    Case = "S"
+                        For i = LBound(cardCheck) To UBound(cardCheck)
+                            cardCheck(i) = Nothing
+                            shuffle = True
                         Next
-                    Next
-                Case = "N"
-                    shuffle = True
-                    shuffleCheck = ""
-            End Select
-
-        Loop Until shuffle = True
-
-        Console.ReadLine()
+                    Case = "Q"
+                        quit = True
+                        shuffle = True
+                    Case <> "S"
+                        Console.WriteLine("Enter S or Q")
+                End Select
+            Loop Until shuffle = True
+        Loop Until quit = True
     End Sub
 
     Function DealACard(max As Integer) As Integer
@@ -114,25 +112,5 @@ Module ShuffleTheDeck
         NumRnd = CInt(rndNum)
         Return NumRnd
     End Function
-
-    Sub TestRandom()
-        Dim randomNumbers(6) As Integer
-
-        For i = 0 To 10000
-
-            randomNumbers(DealACard(6)) += 1
-        Next
-
-        For i = LBound(randomNumbers) To UBound(randomNumbers)
-            Console.Write(CStr(i).PadLeft(7))
-        Next
-        Console.WriteLine()
-        Console.WriteLine(StrDup(80, "-"))
-
-
-        For i = LBound(randomNumbers) To UBound(randomNumbers)
-            Console.Write(CStr(randomNumbers(i)).PadLeft(7))
-        Next
-    End Sub
 End Module
 'https://elearn.isu.edu/moodle/mod/assign/view.php?id=2180761
